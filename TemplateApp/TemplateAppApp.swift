682b5381867f6d96ccab305fb1b26c8eed8ca42a
Nick Kibysh <nick.kibysh@gmail.com>
2023-01-12 12:14:56 +0100

    feat(project): Add Project

---
+++ TemplateApp/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  TemplateApp
+//
+//  Created by Nick Kibysh on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
