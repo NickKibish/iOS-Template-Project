ad5551b5a02bad4fbc81748b06440acb2aaece98
Nick Kibysh <nick.kibysh@gmail.com>
2023-02-21 16:39:48 +0100

    added script for creating keychain

---
+++ ci/install_certificates_and_profiles.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Installing certificates and profiles..."
+
+echo "Decode certificates and profiles"
+
+echo $SIGN_CER_BASE64 | base64 --decode -i - > certificate.cer 
+echo $SIGN_KEY_BASE64 | base64 --decode -i - > key.p12
+echo $SIGN_PROV_PROFILE_BASE64 | base64 --decode -i - > profile.mobileprovision
+
+echo "Create Keychain and import certificates and profiles"
+KEYCHAIN_PATH=~/Library/Keychains/${TEMP_KEYCHAIN_USER}.keychain
+security create-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security unlock-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security import certificate.cer -t cert -k "$TEMP_KEYCHAIN_USER" 
+security import key.p12 -t pkcs12 -k "$TEMP_KEYCHAIN_USER"
+security set-key-partition-list -S apple-tool:,apple: -s -k $TEMP_KEYCHAIN_PASSWORD $KEYCHAIN_PATH
+
+security list-keychains
