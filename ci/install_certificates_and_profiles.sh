98ee9eaa7e51fea2c3da7c0230189a525947fcc1
Nick Kibysh <nick.kibysh@gmail.com>
2023-02-21 16:54:05 +0100

    setup-keychain script modification

--- ci/install_certificates_and_profiles.sh
+++ ci/install_certificates_and_profiles.sh
@@ -13,8 +13,9 @@ KEYCHAIN_PATH=${TEMP_KEYCHAIN_USER}.keychain
 
 security create-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 security list-keychains
-sucurity default-keychain -s "$KEYCHAIN_PATH"
+security default-keychain -s "$KEYCHAIN_PATH"
 security unlock-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security list-keychains
 # security import certificate.cer -t cert -k "$TEMP_KEYCHAIN_USER" 
 # security import key.p12 -t pkcs12 -k "$TEMP_KEYCHAIN_USER"
 # security set-key-partition-list -S apple-tool:,apple: -s -k $TEMP_KEYCHAIN_PASSWORD $KEYCHAIN_PATH
