cbe95bb59d65bdc6a1c168de99bb8bd98df53211
Nick Kibysh <nick.kibysh@gmail.com>
2023-02-21 16:51:44 +0100

    setup-keychain script modification

--- ci/install_certificates_and_profiles.sh
+++ ci/install_certificates_and_profiles.sh
@@ -9,7 +9,8 @@ echo $SIGN_KEY_BASE64 | base64 --decode -i - > key.p12
 echo $SIGN_PROV_PROFILE_BASE64 | base64 --decode -i - > profile.mobileprovision
 
 echo "Create Keychain and import certificates and profiles"
-KEYCHAIN_PATH=~/Library/Keychains/${TEMP_KEYCHAIN_USER}.keychain
+KEYCHAIN_PATH=${TEMP_KEYCHAIN_USER}.keychain
+
 security create-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 security list-keychains
 security unlock-keychain -p "$TEMP_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
