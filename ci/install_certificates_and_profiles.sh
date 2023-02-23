10a7953493f158a9faffeca12b5ac2aee8e0480b
Nick Kibysh <nick.kibysh@gmail.com>
2023-02-23 12:40:27 +0100

    removed 3rd party action

--- ci/install_certificates_and_profiles.sh
+++ ci/install_certificates_and_profiles.sh
@@ -30,11 +30,12 @@ echo "List keychains:"
 security list-keychains
 
 echo "Import certificates"
-security import apple.cer -t cert 
-security import certificate.cer -t cert -k 
-security import key.p12 -t priv -P ""
+security import apple.cer -t cert -A
+security import certificate.cer -t cert -k -A 
+security import key.p12 -t priv -P "" -A 
+
 security list-keychains -d user -s $TEMP_KEYCHAIN_USER
-# security set-key-partition-list -S apple:,apple-tool: -s -k $TEMP_KEYCHAIN_PASSWORD 
+security set-key-partition-list -S apple:,apple-tool: -s -k $TEMP_KEYCHAIN_PASSWORD 
 
 echo "Import provisioning profile"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
